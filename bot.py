20802e568c5a80116c12f204d561854264f80f61
Raoni Godinho <raonigodinho@gmail.com>
2020-09-15 00:06:14 -0300

    Melhorias nos dados e parametrização de especialidade e dificuldade

--- bot.py
+++ bot.py
@@ -4,27 +4,36 @@ import random
 import rolador
 
 import discord
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
+from discord.ext import commands
 
-#load_dotenv()
-TOKEN = 'NzU0NDc1OTk4NTk0MjAzNzMy.X11Shw.r4bVHptMlHU7jeiLTqS57EsIgMQ'
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
     
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('!d'):
         resposta = rolador.interpretadorDado(message.content.split(" "))
         await message.channel.send(resposta)
 
-client.run(TOKEN)
+bot.run(TOKEN)
 
 
--- rolador.py
+++ rolador.py
@@ -2,18 +2,33 @@ import random
 
 random.seed(None)
 
+especializado = False
+target = 6
+
+
 def rolar():
-    return random.randrange(1,10)
+    return random.randint(1,10)
 
 def sucessos(array):
     qtdeSucessos = 0
     for x in array:
-        if (x >=6): 
+        if ((x == 10) and especializado): 
+            qtdeSucessos = qtdeSucessos + 2
+        elif (x >= target): 
             qtdeSucessos = qtdeSucessos + 1
-        elif (x == 1):
-            qtdeSucessos = qtdeSucessos - 1
-    if (qtdeSucessos < 0):
+    
+    qtdeFalhas = array.count(1)
+
+    if (qtdeFalhas > qtdeSucessos):
+        if (qtdeSucessos == 0):
+            qtdeSucessos = 0 - qtdeFalhas
+        else:
+            qtdeSucessos = 0
+    elif (qtdeFalhas == qtdeSucessos):
         qtdeSucessos = 0
+    elif (qtdeFalhas < qtdeSucessos):
+        qtdeSucessos = qtdeSucessos - qtdeFalhas
+
     return qtdeSucessos
 
 def explodir(array):
@@ -24,7 +39,8 @@ def explodir(array):
 
 def formatarResposta(array):
     resposta = "Resultado: "
-    strSucessos = "Sucessos: " + str(sucessos(array)) + "."
+    qtdeSucessos = sucessos(array)
+    strSucessos = "Sucessos: " + str(qtdeSucessos) + "."
     contador = 0
     tamanho = len(array)
     while (contador < tamanho):
@@ -35,6 +51,11 @@ def formatarResposta(array):
             resposta = resposta + ". "
         contador = contador + 1
     resposta = resposta + strSucessos
+    if (qtdeSucessos == 0):
+        resposta = resposta + " Falha!"
+    elif (qtdeSucessos < 0):
+        resposta = resposta + " FALHA CRÍTICA!!"
+
     return resposta
 
 def rolard10(qtde):
@@ -43,17 +64,31 @@ def rolard10(qtde):
     while (contador < qtde):
         array.append(rolar())
         contador = contador + 1
-    array = explodir(array)
+    #array = explodir(array)
     return array
 
-def interpretadorDado(messageArray):
-    if messageArray[0] != '!d':
-        return 'Comando incorreto.'
-    if (len(messageArray) > 1 and messageArray[1].isnumeric()):
-        messageArray = rolard10(int(messageArray[1]))
-    else:
-        messageArray = rolard10(1)
-    return formatarResposta(messageArray)
+def dificuldade(array):
+    global target
+    for x in array:
+        if (x.count("-t")):
+            target = x
+    target = int(target.replace("-t",""))
 
+def interpretadorDado(messageArray):
+    try:
+        if messageArray[0] != '!d':
+            return 'Comando incorreto.'
+        if (messageArray.count("-s")):
+            global especializado 
+            especializado = True
+        dificuldade(messageArray)
+        if (len(messageArray) > 1 and messageArray[1].isnumeric()):
+            messageArray = rolard10(int(messageArray[1]))
+        else:
+            messageArray = rolard10(1)
+        return formatarResposta(messageArray)
+    except:
+        return 'Comando incorreto.'
+        
 
 
